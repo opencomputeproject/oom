ba1882fb43b5297867271c2948c3d1fd0b62af53
Don Bollinger <don@thebollingers.org>
2016-03-29 18:53:39 -0700

    New OOM test, oomtest.py reads every key on every port If it doesn't blow up (get an exception), then at least all of the keys are properly coded and all of the code that reads and writes keys and functions seems to be working.

---
+++ oom/oomtest.py
@@ -0,0 +1,30 @@
+# /////////////////////////////////////////////////////////////////////
+#
+#  oomtest.py :
+#  For each port,
+#      for read keys, functions, write keys
+#          Read each key, read each function, write each write key
+#
+#  Not checking values, just making sure every key is coded in a
+#  way that doesn't throw an exception.
+#
+#  Copyright 2015  Finisar Inc.
+#
+#  Author: Don Bollinger don@thebollingers.or/
+#
+# ////////////////////////////////////////////////////////////////////
+
+from oom import *                   # the published OOM Northbound API
+from decode import hexstr           # helper function from the decode pack
+
+
+portlist = oom_get_portlist()
+for port in portlist:
+    for key in port.mmap:
+        test = oom_get_keyvalue(port, key)
+    for key in port.fmap:
+        test = oom_get_memory(port, key)
+    for key in port.wmap:  # read current value, write it back
+        val = oom_get_keyvalue(port, key)
+        test = oom_set_keyvalue(port, key, val)
+    print '%d raw memory reads for port %s' % (port.readcount, port.port_name)
