0b6be112eec874152f94a3ab212df1ef18eb74ac
Don Bollinger <don@thebollingers.org>
2016-04-21 18:40:48 -0700

    Pep8 cleanup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 setup(
-    name = "oom",
-    version = "0.3",
-    description = "Open Optical Monitoring",
-    url = "https://github.com/ocpnetworking-wip/oom.git",
-    author = "Don Bollinger",
-    author_email = "don@thebollingers.org",
-    license = "MIT",
-    packages = find_packages(),
-    zip_safe = False
+    name="oom",
+    version="0.3",
+    description="Open Optical Monitoring",
+    url="https://github.com/ocpnetworking-wip/oom.git",
+    author="Don Bollinger",
+    author_email="don@thebollingers.org",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=False
 )
