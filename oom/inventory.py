84889d3b66b6007344dcbca0332021bab08d7cdf
Don Bollinger <don@thebollingers.org>
2016-04-27 11:54:02 -0700

    pep8 cleanup for inventory.py

--- oom/inventory.py
+++ oom/inventory.py
@@ -15,9 +15,9 @@ for port in oom_get_portlist():
     modtype = type_to_str(port.port_type)
     if modtype == 'UNKNOWN':
         modtype = 'No Module'
-    print formatstr % (port.port_name, \
-            oom_get_keyvalue(port, 'VENDOR_NAME'), \
-            modtype, \
-            oom_get_keyvalue(port, 'VENDOR_PN'), \
-            oom_get_keyvalue(port, 'VENDOR_SN'))
+    print formatstr % (port.port_name,
+                       oom_get_keyvalue(port, 'VENDOR_NAME'),
+                       modtype,
+                       oom_get_keyvalue(port, 'VENDOR_PN'),
+                       oom_get_keyvalue(port, 'VENDOR_SN'))
 print
