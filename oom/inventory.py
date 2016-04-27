1abd33fb573c77cef3f9978d135c250a9fa3ab5e
Don Bollinger <don@thebollingers.org>
2016-04-27 11:37:25 -0700

    Add inventory.py, to inventory all of the ports on a switch

---
+++ oom/inventory.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+#
+# inventory.py
+#
+# OOM script to inventory the modules in a switch
+#
+from oom import *
+from oomlib import type_to_str
+
+formatstr = '%-10s %-16s %-13s %-16s %-16s'
+print
+print formatstr % ('Port Name', 'Vendor', 'Type', 'Part #', 'Serial #')
+print
+for port in oom_get_portlist():
+    modtype = type_to_str(port.port_type)
+    if modtype == 'UNKNOWN':
+        modtype = 'No Module'
+    print formatstr % (port.port_name, \
+            oom_get_keyvalue(port, 'VENDOR_NAME'), \
+            modtype, \
+            oom_get_keyvalue(port, 'VENDOR_PN'), \
+            oom_get_keyvalue(port, 'VENDOR_SN'))
+print
